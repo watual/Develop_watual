8088ce000b76a56dd3aa230f799d6601b7001c3b
watual <qordnjsgk123455@gmail.com>
2024-03-30 22:27:08 +0900

    Update java

--- java/test1/test1/src/Main.java
+++ java/test1/test1/src/Main.java
@@ -1,14 +1,12 @@
-import java.util.Scanner;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-
         int n = scan.nextInt();
 
-        for (int i = 1; i < 10; i++) {
-            System.out.println(n + " * " + i + " = " + n * i);
-        }
+        System.out.println((n + 1) * n / 2);
 
         scan.close();
     }
