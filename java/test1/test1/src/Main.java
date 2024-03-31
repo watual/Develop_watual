d0c1445b9a3706c034d5ab2d7ba1288e255b8c22
watual <qordnjsgk123455@gmail.com>
2024-04-01 00:17:54 +0900

    Update java 코드기록

--- java/test1/test1/src/Main.java
+++ java/test1/test1/src/Main.java
@@ -5,31 +5,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        int n = 0, basket = 0;
+        int count = 0, result=1;
+        int[] num = new int[10];
 
-        st = new StringTokenizer(br.readLine());
-        basket = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
-
-        int[] balls = new int[basket];
-
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int num = Integer.parseInt(st.nextToken());
-            for (int j = start; j <= end; j++) {
-                balls[j-1] = num;
+        for (int i = 0; i < 10; i++) {
+            num[i] = Integer.parseInt(br.readLine()) % 42;
+            count=0;
+            for (int j = 0; j < i; j++) {
+                if (num[i] != num[j]) {
+                    count++;
+                }
+                if(count>=i){
+                    result++;
+                }
             }
         }
-
-        for (int i = 0; i < basket; i++) {
-            bw.write(balls[i] + "");    //버퍼에 쓰이는 데이터는 string으로 나가기 때문에 자료형을 바꿔줘야한다
-            if (i < basket - 1) {
-                bw.write(" ");
-            }
-        }
+        bw.write(String.valueOf(result));
         bw.write("\n");
         bw.close();
     }
