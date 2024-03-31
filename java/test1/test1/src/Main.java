4e469842f86f87d150e654069edaa04a52ab9288
watual <qordnjsgk123455@gmail.com>
2024-03-31 13:55:37 +0900

    Update java

--- java/test1/test1/src/Main.java
+++ java/test1/test1/src/Main.java
@@ -7,10 +7,13 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
+
         int n = Integer.parseInt(br.readLine());
-        for (int i = 0; i < n; i++) {
+
+        for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
-            bw.write(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()) + "\n");
+            int k = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
+            bw.write("Case #" + i + ": " + k + "\n");
             // bw.flush();
         }
         bw.close();
