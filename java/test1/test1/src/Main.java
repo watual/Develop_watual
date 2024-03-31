614c5ad44587e241118eba63652c2c910740a2b9
watual <qordnjsgk123455@gmail.com>
2024-03-31 15:22:02 +0900

    Update java 코드기록 백준문제풀이

--- java/test1/test1/src/Main.java
+++ java/test1/test1/src/Main.java
@@ -1,5 +1,9 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -12,10 +16,24 @@ public class Main {
 
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
-            int k = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
-            bw.write("Case #" + i + ": " + k + "\n");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write("Case #" + i + ": " + a + " + " + b + " = " + (a + b) + "\n");
             // bw.flush();
         }
         bw.close();
+        // ==================================================
+        // Scanner s = new Scanner(System.in);
+        // int n = s.nextInt();
+        // int[] a = new int[n];
+        // int[] b = new int[n];
+        // for (int i = 0; i < n; i++) {
+        // a[i] = s.nextInt();
+        // b[i] = s.nextInt();
+        // }
+        // for (int i = 0; i < n; i++) {
+        // System.out.println("Case #" + (i + 1) + ": " + a[i] + " + " + b[i] + " = " +
+        // (a[i] + b[i]));
+        // }
     }
 }
