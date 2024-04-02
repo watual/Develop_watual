a12f2d6318a1d141154b6472b854c8ca3418eabe
watual <qordnjsgk123455@gmail.com>
2024-04-03 05:17:55 +0900

    새로운 테스트

--- java/test1/test1/src/Main.java
+++ java/test1/test1/src/Main.java
@@ -3,15 +3,24 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        int n = Integer.parseInt(br.readLine());
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            bw.write(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()) + "\n");
-            // bw.flush();
+
+        Scanner sc = new Scanner(System.in);
+
+        int B = sc.nextInt();
+        String N = sc.nextLine().trim(); // zzzzz
+        int[] ju = new int[N.length()]; // {35, 35, 35, 35, 35}
+
+        for (int i = 0; i < N.length(); i++) { // a+25 97+25 122>35부터 시작합니다~ A 65
+            ju[i] = (int) N.charAt(i) - 87;
+            // {10, 11, 12, 13, 14}
+            // {a, b, c, d, e}
         }
-        bw.close();
+
+        int cnt = 0;
+        for (int i = 0; i < N.length(); i++) {
+            cnt += (int) Math.pow(B, (N.length() - i - 1)) * ju[i];
+        }
+        System.out.print(cnt);
+        sc.close();
     }
 }
